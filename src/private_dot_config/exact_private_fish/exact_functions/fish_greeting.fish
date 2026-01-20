e61b3b3c373f34021da21f971daaeafdfdd13907
m0r15 <serenkovav@gmail.com>
2026-01-20 19:18:47 +0300

    Add fish greeting

---
+++ src/private_dot_config/exact_private_fish/exact_functions/fish_greeting.fish
@@ -0,0 +1,7 @@
+function fish_greeting
+    echo " __  __  ___  ____  __  ___"
+    echo "(  \/  )/ _ \(  _ \/  )| __)  | Aleksei Serenkov (m0r15)"
+    echo " )    (( (_) ))   / )( |__ \  | serenkovav@gmail.com"
+    echo "(_/\/\_)\___/(_)\_)(__)(___/  |"
+    echo 
+end
