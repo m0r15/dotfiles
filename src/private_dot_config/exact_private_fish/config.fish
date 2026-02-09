b3cbe31f3be4a4022b53544e9f1589d316dbc7cc
m0r15 <serenkovav@gmail.com>
2026-02-09 16:39:14 +0300

    Add aliases

--- src/private_dot_config/exact_private_fish/config.fish
+++ src/private_dot_config/exact_private_fish/config.fish
@@ -1,15 +1,19 @@
-# Export PATH
+if [ -e ~/.config/fish/aliases.fish ]
+    . ~/.config/fish/aliases.fish
+end
+
+# Globals {{{
 fish_add_path /opt/homebrew/bin
 fish_add_path $HOME/.local/bin
 fish_add_path $HOME/bin
 
 set -gx EDITOR vim
 set -gx VISUAL vim
+#}}}
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# Aliases
-abbr --add D docker
-abbr --add DR docker run
+
+
